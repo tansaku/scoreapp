d57795068f07689aefdaf002f9d94c43abb715ed
Sam Joseph <tansaku@gmail.com>
2014-04-02 19:14:20 +0100

    adding some server scores

--- scoreapp.js
+++ scoreapp.js
@@ -1,3 +1,9 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to scoreapp.";
@@ -15,5 +21,15 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Players.find().count() === 0) {
+      var names = ["Rene Paulokat",
+                   "Sam Joseph",
+                   "Jon Mohrbacher",
+                   "Bill Walker",
+                   "Julian Alvarez"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    }
+
   });
 }
